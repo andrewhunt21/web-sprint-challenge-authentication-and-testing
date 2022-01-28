200aea822b692a85758874166aa5f14d11aab890
andrewhunt21 <andrewhunt21@icloud.com>
2022-01-27 16:24:08 -0800

    basic sanity check reading through mvp

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,15 @@
 // Write your tests here
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+describe('')
