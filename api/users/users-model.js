ae01eb582b6b06e87166093b5cc8aae73f9d00fa
andrewhunt21 <andrewhunt21@icloud.com>
2022-01-28 11:10:03 -0800

    users model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -8,14 +8,19 @@ function findBy(filter) {
     return db('users').where(filter)
 }
 
+function findById(user_id) {
+    return db('users')
+        .where('user_id', user_id).first()
+}
+
 async function add(user) {
     const [id] = await db('users').insert(user)
-    return db('users')
-    .where('user_id', id).first()
+    return findById(id)
 }
 
 module.exports = {
     find,
     findBy,
+    findById,
     add
 }
