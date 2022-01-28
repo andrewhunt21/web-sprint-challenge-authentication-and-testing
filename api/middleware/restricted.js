e7d514eb00c38b43814cb81c1907570d74d54943
andrewhunt21 <andrewhunt21@icloud.com>
2022-01-28 12:33:33 -0800

    restricted

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -4,16 +4,17 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
   if (!token) {
-    return next({ status: 401, message: 'token required' })
+    next({ status: 401, message: 'token required' })
+  } else {
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        next({ status: 401, message: 'token invalid' })
+      } else {
+        req.decodedToken = decodedToken
+        next()
+      }
+    })
   }
-  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
-    if (err) {
-      next({ status: 401, message: 'token invalid' })
-    } else {
-      req.decodedToken = decodedToken
-      next()
-    }
-  })
   /*
     IMPLEMENT
 
